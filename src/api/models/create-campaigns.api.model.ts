11c2a8ad26e054f583b636e08458dc3793b2febd
Piotr <piotr@pulsatehq.com>
2024-11-14 14:58:13 +0100

    Adding factory and models for segments and campaign

---
+++ src/api/models/create-campaigns.api.model.ts
@@ -0,0 +1,94 @@
+export interface CampaignGoal {
+  primary: boolean;
+  event_kind: string;
+  event_identifier: string | null;
+  expiry_time_unit: string;
+  expiry_time_value: number;
+}
+
+export interface CampaignButton {
+  label: string;
+  destination_type: string;
+  txt_color: string;
+  btn_color: string;
+}
+
+export interface CampaignCallToAction {
+  buttons: CampaignButton[];
+  side: string;
+}
+
+export interface CampaignText {
+  side: string;
+  active: boolean;
+  text: string;
+  position: number;
+}
+
+export interface CampaignImage {
+  side: string;
+  name: string;
+  active: boolean;
+  position: number;
+  url: string;
+}
+
+export interface CampaignHeadline {
+  side: string;
+  active: boolean;
+  text: string;
+  position: number;
+}
+
+export interface CampaignAdminHeaderWithMessage {
+  side: string;
+  position: number;
+  active: boolean;
+  message: string;
+  admin: {
+    job_title: string | null;
+    name: string | null;
+    avatar_url: string | null;
+    s_id: string | null;
+  };
+}
+
+export interface CampaignLargeNotification {
+  admin_header_with_message: CampaignAdminHeaderWithMessage;
+  image: CampaignImage;
+  headline: CampaignHeadline;
+  text: CampaignText;
+  call_to_action: CampaignCallToAction;
+}
+
+export interface InAppNotification {
+  large: CampaignLargeNotification;
+}
+
+export interface CreateCampaignPayload {
+  state_machine_notifications_state: string;
+  duplication_source_id: string;
+  type: string;
+  last_builder_page: string;
+  name: string;
+  control_group: null;
+  goals: CampaignGoal[];
+  in_app_notification: InAppNotification;
+  segment_ids: string[];
+  beacon_ids: string[];
+  geofence_ids: string[];
+  in_app_event_names: string[];
+  beacon_events: Record<string, unknown>;
+  geofence_events: Record<string, unknown>;
+  geofence_dwelling_times: Record<string, unknown>;
+  start_now: boolean;
+  start_at: string;
+  end_at: string;
+  time_frame: string;
+  time_value: string;
+  time_zone_name: string;
+  time_zone_offset: string;
+  delivery: string;
+  campaign_limits: boolean;
+  campaign_expiry: boolean;
+}
--- src/api/utils/api.util.ts
+++ src/api/utils/api.util.ts
@@ -3,6 +3,7 @@ import { API_E2E_APP_ID, BASE_URL, SDK_API_URL } from '@_config/env.config';
 export const apiUrls = {
   importUsersUrl: `${BASE_URL}/api/v2/import_users_for_single_app`,
   segmentsUrlV1: `${BASE_URL}/api/v1/apps/${API_E2E_APP_ID}/segments`,
+  segmentsUrlV2: `${BASE_URL}/api/v2/apps/${API_E2E_APP_ID}/segments`,
   campaignsUrlV2: `${BASE_URL}/api/v2/apps/${API_E2E_APP_ID}/campaigns`,
   usersUrlV2: `${BASE_URL}/api/v2/apps/${API_E2E_APP_ID}/users`,
   startSessionUrlV4: `${SDK_API_URL}/api/mobile/v4/sessions/start`,
